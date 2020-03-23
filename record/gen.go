d77e687403aada420b73a8dd29a302c4c31bb937
Jeremías <jrmsdev@gmail.com>
2020-03-23 20:31:45 -0300

    do not cheat benchmarks

--- record/gen.go
+++ record/gen.go
@@ -16,7 +16,7 @@ var tsStart int64 = 1582156800
 var logfmt = `{"level":"debug","ts":%d,"conn_id":%d,"state":"closed","Tx":%d,"Rx":%d}`
 var xmax = 10000
 
-// Gen populate log file with random data
+// generate log data, if p is true print it to stdout too
 func genlog(p bool) {
 	tsEnd := time.Now().UTC().Unix()
 	rand.Seed(tsEnd)
@@ -24,16 +24,21 @@ func genlog(p bool) {
 	//~ tsStart = tsEnd - 2 // FIXME
 
 	for x := tsStart; x <= tsEnd; x++ {
+		// tx and rx are generated here so we don't cheat the benchmarks
+		tx := rand.Intn(xmax)
+		rx := rand.Intn(xmax)
 		if p {
-			fmt.Printf(logfmt + "\n", tsStart + x, x, rand.Intn(xmax), rand.Intn(xmax))
+			fmt.Printf(logfmt + "\n", tsStart + x, x, tx, rx)
 		}
 	}
 }
 
+// Gen generates log data but do not prints it.
 func Gen() {
 	genlog(false)
 }
 
+// Print generates log data and prints it to stdout.
 func Print() {
 	genlog(true)
 }
